0bf161a50888525cbc3ed088f5ecd53a7f0bc79a
AKULANATESH <nateshakula7372@gmail.com>
2024-05-18 13:40:21 +0530

    added vallidation with email  and search user thro ugh name

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,31 +1,45 @@
-import { Controller, Get, Post , Put, Delete, Body, Param} from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
 
 import { UserService } from './user.service';
 import { User } from './user.types';
 
-@Controller("user")
-export class UserController{
+@Controller('user')
+export class UserController {
   constructor(private readonly userService: UserService) {}
-
+  //  @Get()
+  //  getUsers()
   @Get()
-  getUser(): string {
-    return this.userService.getUser();
+  getUsers(): string {
+    return this.userService.getUsers();
   }
 
-  @Post("add")
-  async addUser(@Body() userData: User): Promise<User> {
-    const result = this.userService.addUser(userData);
-    return result;
-  };
-  
-  @Put("update")
-  updateUser(@Body() userData: User):string{
+  @Get('/:name')
+  async getUser(@Param('name') userName: string): Promise<User | string> {
+    return this.userService.getUser(userName);
+  }
+
+  @Post('add')
+  async addUser(@Body() userData: User): Promise<string | User> {
+    return this.userService.addUser(userData);
+  }
+
+  @Put('update')
+  updateUser(@Body() userData: User): string {
     return this.userService.updateUser(userData);
   }
 
-  @Delete("delete/:id")
-  async deleteUser( @Param('id') userId: number):Promise<string> {
-    const result = this.userService.deleteUser(userId);
+  @Delete('delete/:id')
+  async deleteUser(@Param('id') userId: string): Promise<string> {
+    const result = this.userService.deleteUser(parseInt(userId));
     return result;
   }
-} 
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,38 +1,56 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { User } from './user.types';
 
-  @Injectable()
-  export class UserService {
+@Injectable()
+export class UserService {
   usersList: User[] = [];
 
-  getUser(): any{
-    // return only individual user
+  getUsers(): any {
     return this.usersList;
   }
 
-  addUser(newUser: User): User {
+  getUser(userName: string): User | string {
+    const user = this.usersList.find((user) => user.name === userName);
+    if (user) {
+      return user;
+    } else {
+      return 'user not found';
+    }
+    // return only individual user
+  }
+
+  addUser(newUser: User): User | string {
+    const Emailexists = this.usersList.find(
+      (user) => user.email === newUser.email,
+    );
+    if (Emailexists) {
+      return ' email already exists ';
+    }
     // validation user email already exists
-    this.usersList.push(newUser)
+    this.usersList.push(newUser);
     return newUser;
   }
 
-  updateUser(updatedUser: User): string{
-    const userIdx = this.usersList.findIndex(user => user.id === updatedUser.id);
-    if (userIdx !== -1){
+  updateUser(updatedUser: User): string {
+    const userIdx = this.usersList.findIndex(
+      (user) => user.id === updatedUser.id,
+    );
+    if (userIdx !== -1) {
       this.usersList[userIdx] = { ...this.usersList[userIdx], ...updatedUser };
-      return "User updated successfully!";
+      return 'User updated successfully!';
     } else {
-      return "User not found!";
+      return 'User not found!';
     }
   }
 
   deleteUser(userId: number): string {
-    const userIndex = this.usersList.findIndex(user => user.id === userId);
+    const userIndex = this.usersList.findIndex((user) => user.id === userId);
     if (userIndex !== -1) {
       this.usersList.splice(userIndex, 1);
-      return "User deleted successfully!";
+      return 'User deleted successfully!';
     } else {
-      return "User not found!";
+      return 'User not found!';
     }
   }
 }
